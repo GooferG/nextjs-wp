ddd6343c38e392e05a24a4d8394b721f7f5b1fc4
Luiz Meneghim <luiz.meneghim@hotmail.com>
2024-05-01 13:18:25 -0700

    added header

--- components/BlockRenderer/BlockRenderer.js
+++ components/BlockRenderer/BlockRenderer.js
@@ -1,12 +1,25 @@
 import { Cover } from "components/Cover";
+import { Heading } from "components/Heading";
 
 export const BlockRenderer = ({ blocks }) => {
   return blocks.map((block) => {
     switch (block.name) {
+      case "core/heading": {
+        console.log("BLOCK:", block);
+        return (
+          <Heading
+            key={block.id}
+            level={block.attributes.level}
+            content={block.attributes.content}
+            textAlign={block.attributes.textAlign}
+          />
+        );
+      }
       case "core/cover": {
+        console.log("BLOCK:", block);
         return (
           <Cover key={block.id} background={block.attributes.url}>
-            core cover
+            <BlockRenderer blocks={block.innerBlocks} />
           </Cover>
         );
       }
